62c58bada5b8cc81577d748a4ea7f5b414175028
ctcommon <ctcommon@163.com>
2018-01-16 15:12:49 +0800

    多线程的测试

---
+++ thread.py
@@ -0,0 +1,17 @@
+import threading
+
+def print_work(cunt):
+	for i in range(cunt):
+		print 'new thread print:', i
+
+def main():
+	t = threading.Thread(target=print_work,args=(10,))
+	t.start()
+	sum=0
+	for i in range(100):
+		sum += i
+	print 'sum=%s' %sum
+
+if __name__ == "__main__":
+	main()
+	
